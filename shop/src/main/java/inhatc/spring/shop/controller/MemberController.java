a1cfb57dd26dbf749d6ed20dcaa0c26cb4b2214c
Pinkippo <hseungwan8@gmail.com>
2023-10-18 11:28:33 +0900

    feat(week8): 실습파일
    - 회원가입 기능 구현

--- shop/src/main/java/inhatc/spring/shop/controller/MemberController.java
+++ shop/src/main/java/inhatc/spring/shop/controller/MemberController.java
@@ -1,20 +1,38 @@
 package inhatc.spring.shop.controller;
 
+import inhatc.spring.shop.dto.MemberFormDto;
+import inhatc.spring.shop.entity.Member;
 import inhatc.spring.shop.service.MemberService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller()
 public class MemberController {
 
     private final MemberService memberService;
 
-    public MemberController(MemberService memberService) {
+    private final PasswordEncoder passwordEncoder;
+
+    public MemberController(MemberService memberService, PasswordEncoder passwordEncoder) {
         this.memberService = memberService;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping("member/new")
-    public String MemberForm() {
+    @GetMapping(value = "member/new")
+    public String memberForm(Model model) {
+        model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
     }
+
+    @PostMapping("member/new")
+    public String insertMember(MemberFormDto memberFormDto) {
+
+        Member member = Member.createMember(memberFormDto,passwordEncoder);
+        memberService.saveMember(member);
+        return "redirect:/"; // 회원가입 후 메인화면으로 이동
+    }
+
 }
